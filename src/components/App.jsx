cba4fc8ebe126362b8e7ea2ad2a0356aa448fa17
Vadym <aspirantmpm@gmail.com>
2023-03-16 21:23:21 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,74 @@
-import { Component } from 'react';
-// import React, { useState } from 'react';
+// import { Component } from 'react';
+import React, { useState } from 'react';
 import { GlobalStyle } from './Globalstyle';
 import { Section } from './Section';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onChangeGood = itemGood => {
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // };
+
+  const onChangeGood = itemGood => {
     // console.log(itemGood);
-    this.setState({ good: itemGood });
+    setGood( itemGood );
   };
 
-  onChangeNeutral = itemNeutral => {
+  const onChangeNeutral = itemNeutral => {
     // console.log(itemNeutral);
-    this.setState({ neutral: itemNeutral });
+    setNeutral( itemNeutral );
   };
 
-  onChangeBad = itemBad => {
+  const onChangeBad = itemBad => {
     // console.log(itemBad);
-    this.setState({ bad: itemBad });
+    setBad( itemBad );
   };
 
-  countTotalFeedback = () => {
-    let totalFeedback  = this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    let totalFeedback = good + neutral + bad;
     // console.log(totalFeedback)
-    return totalFeedback;    
+    return totalFeedback;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    let positiveFeedback =
-        Math.round((this.state.good * 100) / this.countTotalFeedback());
-      // console.log(positiveFeedback)
-    if (this.state.good !== 0) {
-      return positiveFeedback; 
-    }  else return '0'
-  
-}
-  
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <GlobalStyle />
-        <Section
-          itemGood={this.state.good}
-          itemNeutral={this.state.neutral}
-          itemBad={this.state.bad}
-          onChangeGood={this.onChangeGood}
-          onChangeNeutural={this.onChangeNeutral}
-          onChangeBad={this.onChangeBad}
-          feedback={this.countTotalFeedback}
-          positive={this.countPositiveFeedbackPercentage}
-        />
-      </div>
+  const countPositiveFeedbackPercentage = () => {
+    let positiveFeedback = Math.round(
+      (good * 100) / countTotalFeedback()
     );
-  }
-}
+    // console.log(positiveFeedback)
+    if (good !== 0) {
+      return positiveFeedback;
+    } else return '0';
+  };
+
+  // render() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <GlobalStyle />
+      <Section
+        itemGood={good}
+        itemNeutral={neutral}
+        itemBad={bad}
+        onChangeGood={onChangeGood}
+        onChangeNeutural={onChangeNeutral}
+        onChangeBad={onChangeBad}
+        feedback={countTotalFeedback}
+        positive={countPositiveFeedbackPercentage}
+      />
+    </div>
+  );
+  // }
+};
